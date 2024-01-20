6e50256595ddf5d8a99ee2f2c160d4533f8bdd9e
Enes A <enesaksay06@gmail.com>
2024-01-20 22:50:51 +0300

    Set up project

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "InterBlack": require("./assets/fonts/InterBlack.ttf"),
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>Wi-Fi P2P Prototype</Text>
     </View>
   );
 }
@@ -17,4 +20,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontFamily: "InterBlack",
+    fontSize: 30,
+  }
 });
